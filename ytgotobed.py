5469eb8db8bfdecdf8fa18c8c1fd7f3b8829bbce
etisHere <76783922+etisHere@users.noreply.github.com>
2021-03-26 01:31:51 -0400

    FV

--- ytgotobed.py
+++ ytgotobed.py
@@ -5,4 +5,5 @@ useri = input("What's the url?: ")
 newurl = useri.replace("watch?v=", "embed/")
 webbrowser.open(newurl)
 
+#Paste script in with a alias in bash for quick access (i have it set to yte)
 #et
